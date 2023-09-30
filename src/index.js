c15b216c86a351e6831a1e06155ba4d0364cca53
eatekha <atekhaeseosa@gmail.com>
2023-09-30 11:05:54 -0400

    Charts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'assets/css/App.css';
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
-import AuthLayout from 'layouts/auth';
 import AdminLayout from 'layouts/admin';
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
 import theme from 'theme/theme';
@@ -36,7 +35,6 @@ ReactDOM.render(
 			<ThemeEditorProvider>
 				<HashRouter>
 					<Switch>
-						<Route path={`/auth`} component={AuthLayout} />
 						<Route path={`/admin`} component={AdminLayout} />
 						<Redirect from='/' to='/admin' />
 					</Switch>
--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,6 @@ import MainDashboard from "views/admin/default";
 import NFTMarketplace from "views/admin/marketplace";
 import Profile from "views/admin/profile";
 import DataTables from "views/admin/dataTables";
-import RTL from "views/admin/rtl";
 
 const routes = [
   {
